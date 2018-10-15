f8c93c423cdb670b30557cbab08eff696f7dab65
Rory Claasen <t-roclaa@microsoft.com>
2018-10-15 19:28:53 +0100

    Added base start code

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+	<React.Fragment>
+		<span>App Goes here</span>
+	</React.Fragment>,
+	document.getElementById('root')
+);
