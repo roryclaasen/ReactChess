8238a0194af14a1fefb91887a3a45b0d3065bf0c
Rory Claasen <t-roclaa@microsoft.com>
2019-01-17 15:20:45 +0000

    Added play into express for server when using react router

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ app.use(express.static(path.join(__dirname, '..', 'build')));
 
 app.get([
 	'/',
+	'/play'
 ], (req, res) => {
 	res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
